447068f26042f4606837b9e09c32f299a50148c5
Liam Risdon <liamrisdon22@gmail.com>
2023-11-01 19:27:48 +0000

    login and signup pages added

--- FrontEnd/EliteCare Booking/src/pages/LoginPage.jsx
+++ FrontEnd/EliteCare Booking/src/pages/LoginPage.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
-    return (
-        <div>LoginPage</div>
-    )
+
+    const [formData, setFormData] = useState({
+        email: "",
+        password: ""
+    });
+
+    const handleInputChange = e => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    return <>
+        <section className="px-5 lg:px-0">
+            <div className="w-full max-w-[570px] mx-auto rounded-lg shadow-md md:p-10">
+                <h3 className="text-heading text-[22px] leading-9 font-bold mb-10">Hello! <span className="text-primary">Welcome </span>Back</h3>
+
+                <form className="py-4 md:py-0">
+                    <div className="mb-5">
+                        <input type="email" placeholder="Please enter your email" name="email" value={formData.email} onChange={handleInputChange} className="w-full px-4 py-3 border-b border-solid border-red-400 focus:outline-none focus:border-b-primary text-[22px] leading-7 text-textColor placeholder:text-heading rounded-md cursor-pointer" required />
+                    </div>
+
+                    <div className="mb-5">
+                        <input type="password" placeholder="Please enter your password" name="password" value={formData.password} onChange={handleInputChange} className="w-full px-4 py-3 border-b border-solid border-red-400 focus:outline-none focus:border-b-primary text-[22px] leading-7 text-textColor placeholder:text-heading rounded-md cursor-pointer" required />
+                    </div>
+
+                    <div className="mt-7">
+                        <button type="submit" className="w-full bg-primary text-white text-[18px] leading-[30px] rounded-lg px-4 py-3">Login</button>
+                    </div>
+
+                    <p className="mt-5 text-textColor text-center"> No Account? <Link to="/signup" className="text-primary font-medium ml-1">Register here</Link></p>
+
+                </form>
+            </div>
+
+        </section>
+    </>
 }
 
 export default LoginPage;
--- FrontEnd/EliteCare Booking/src/pages/SignUpPage.jsx
+++ FrontEnd/EliteCare Booking/src/pages/SignUpPage.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const SignUpPage = () => {
-    return (
-        <div>SignUpPage</div>
-    )
+
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+        name: "",
+        gender: "",
+    });
+
+    const handleInputChange = e => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const submitHandler = async e => {
+        e.preventDefault();
+    }
+
+    return <>
+        <section className="px-5 xl:px-0">
+            <div className="max-w-[1170px] mx-auto">
+                <div className="grid grid-cols-1 lg:grid-cols-2">
+                    <div className="rounded-l-lg lg:pl-16 py-10">
+                        <h3 className="text-heading text-[22px] leading-9 font-bold mb-10">
+                            Create an <span className="text-primary">account</span>
+                        </h3>
+
+                        <form onSubmit={submitHandler}>
+                            <div className="mb-5">
+                                <input type="text" placeholder="Full Name" name="name" value={formData.name} onChange={handleInputChange} className="w-full pr-4 px-4 py-3 border-b border-solid border-red-400 focus:outline-none focus:border-b-primary text-[22px] leading-7 text-textColor placeholder:text-heading rounded-md cursor-pointer" required />
+                            </div>
+
+                            <div className="mb-5">
+                                <input type="email" placeholder="Enter your email" name="email" value={formData.email} onChange={handleInputChange} className="w-full pr-4 px-4 py-3 border-b border-solid border-red-400 focus:outline-none focus:border-b-primary text-[22px] leading-7 text-textColor placeholder:text-heading rounded-md cursor-pointer" required />
+                            </div>
+
+                            <div className="mb-5">
+                                <input type="password" placeholder="Password" name="password" value={formData.password} onChange={handleInputChange} className="w-full pr-4 px-4 py-3 border-b border-solid border-red-400 focus:outline-none focus:border-b-primary text-[22px] leading-7 text-textColor placeholder:text-heading rounded-md cursor-pointer" required />
+                            </div>
+
+                            <label className="text-heading font-bold text-[16px] leading-7">
+                                Gender:
+                                <select name="gender" value={formData.gender} onChange={handleInputChange} className="text-textColor font-semibold text-[15px] leading-7 px-4 py-3 focus:outline-none">
+                                    <option value="">Select</option>
+                                    <option value="female">Female</option>
+                                    <option value="male">Male</option>
+                                    <option value="other">Other</option>
+                                </select>
+                            </label>
+
+                            <div className="mt-7">
+                                <button type="submit" className="w-full bg-primary text-white text-[18px] leading-[30px] rounded-lg px-4 py-3">Sign Up</button>
+                            </div>
+
+                            <p className="mt-5 text-textColor text-center"> Already have an Account? <Link to="/login" className="text-primary font-medium ml-1">Login here</Link></p>
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+
+        </section>
+    </>
 }
 
-export default SignUpPage
+export default SignUpPage;
